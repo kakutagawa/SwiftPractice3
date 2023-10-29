1546ddf74c2653ea0334f9a13949350eb2baad8c
kakutagawa <kaktgw0824@gmail.com>
2023-10-29 14:57:53 +0900

    MyJanken3作成

---
+++ MyJanken3/MyJanken3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyJanken3
+//
+//  Created by 芥川浩平 on 2023/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyJanken3/MyJanken3/MyJanken3App.swift
@@ -0,0 +1,17 @@
+//
+//  MyJanken3App.swift
+//  MyJanken3
+//
+//  Created by 芥川浩平 on 2023/10/29.
+//
+
+import SwiftUI
+
+@main
+struct MyJanken3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
