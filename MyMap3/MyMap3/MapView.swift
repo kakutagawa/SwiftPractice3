5ddea59c35d9b3bcde55f8a459098faf0fb9b4fa
kakutagawa <kaktgw0824@gmail.com>
2023-10-31 23:37:26 +0900

    MyMap3完成まで

--- MyMap3/MyMap3/MapView.swift
+++ MyMap3/MyMap3/MapView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 import MapKit
 
+enum MapType {
+    case standard
+    case satellite
+    case hybrid
+}
+
 struct MapView: UIViewRepresentable {
     let searchKey: String
+    let mapType: MapType
 
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
     }
     func updateUIView(_ uiView: MKMapView, context: Context) {
         print("検索キーワード：\(searchKey)")
+
+        switch mapType {
+        case .standard:
+            uiView.preferredConfiguration = MKStandardMapConfiguration(elevationStyle: .flat)
+
+        case .satellite:
+            uiView.preferredConfiguration = MKImageryMapConfiguration()
+
+        case .hybrid:
+            uiView.preferredConfiguration = MKHybridMapConfiguration()
+        }
 
         let geocoder = CLGeocoder()
 
@@ -48,5 +66,5 @@ struct MapView: UIViewRepresentable {
 }
 
 #Preview {
-    MapView(searchKey: "羽田空港")
+    MapView(searchKey: "羽田空港",  mapType: .standard)
 }
