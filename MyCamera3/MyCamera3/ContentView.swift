144a47581b06fcff6c8445f234d0deeba0e19b09
kakutagawa <kaktgw0824@gmail.com>
2023-11-03 00:25:05 +0900

    MyCamera3着手、エラー出て消えない

---
+++ MyCamera3/MyCamera3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyCamera3
+//
+//  Created by 芥川浩平 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
