c5c019a53b95cd1d2013f97117ceb72975f4ec71
kakutagawa <kaktgw0824@gmail.com>
2023-11-04 15:12:39 +0900

    MyCamera3完成

--- MyCamera3/MyCamera3/ContentView.swift
+++ MyCamera3/MyCamera3/ContentView.swift
@@ -6,16 +6,69 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet = false
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Button {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("カメラは利用できます")
+                    captureImage = nil
+                    isShowSheet.toggle()
+                } else {
+                    print("カメラは利用できません")
+                }
+            } label: {
+                Text("カメラを起動する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(.blue)
+                    .foregroundColor(.white)
+            }
+            .padding()
+            .sheet(isPresented: $isShowSheet) {
+                if let captureImage {
+                    EffectView(isShowSheet: $isShowSheet, captureImage: captureImage)
+                } else {
+                    ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+                }
+            }
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                Text("フォトライブラリーから選択する")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(.blue)
+                    .foregroundColor(.white)
+            }
+            .onChange(of: photoPickerSelectedImage) {_, photosPickerItem in
+                if let photosPickerItem {
+                    photosPickerItem.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            if let data {
+                                captureImage = nil
+                                captureImage = UIImage(data: data)
+                            }
+                        case .failure:
+                            return
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .onChange(of: captureImage) {_, image in
+            if let _ = image {
+                isShowSheet.toggle()
+            }
+        }
     }
 }
 
